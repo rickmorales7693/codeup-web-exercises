2e96097d62d0e4fe20a48e98b29579d44c3683c3
Rick Morales <rick.morales7693@gmail.com>
2023-06-28 09:52:05 -0500

    added new warmup in js-warmsup.html

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,53 +1,53 @@
-//#2
-// function showMultiplicationTable(number){
-//     for(let i = 1; i <= 10; ++i){
-//         console.log(`${number} x ${i} = ${number * i}`);
-//     }
-// }
-//
-// showMultiplicationTable(parseInt(prompt("Enter a number")));
+// #2
+function showMultiplicationTable(number){
+    for(let i = 1; i <= 10; ++i){
+        console.log(`${number} x ${i} = ${number * i}`);
+    }
+}
 
-//#3.1
-// for (let i = 0; i < 10; ++i) {
-//     let randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
-//     if (randomNumber % 2 === 0) {
-//         console.log(randomNumber + " is even");
-//     } else {
-//         console.log(randomNumber + " is odd");
-//     }
-// }
+showMultiplicationTable(parseInt(prompt("Enter a number")));
 
-//#3.2
-// function randomEvenOrOdd(){
-//     for(let i = 0; i < 10; ++i){
-//         const randomNumber = Math.floor(Math.random() * (200 - 20) + 1) + 20;
-//         if (randomNumber % 2 === 0) {
-//             console.log(`${randomNumber} is even`);
-//         } else {
-//             console.log(`${randomNumber} is odd`);
-//         }
-//     }
-// }
-// randomEvenOrOdd()
+// #3.1
+for (let i = 0; i < 10; ++i) {
+    let randomNumber = Math.floor(Math.random() * (200 - 20 + 1)) + 20;
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even");
+    } else {
+        console.log(randomNumber + " is odd");
+    }
+}
 
-//#4
-// for (let i = 1; i <= 9; ++i) {
-//     let result = [];
-//     for (a = 1; a <= i; ++a){
-//         result.push(i);
-//     }
-//     console.log(result.join(""));
-// }
+// #3.2
+function randomEvenOrOdd(){
+    for(let i = 0; i < 10; ++i){
+        const randomNumber = Math.floor(Math.random() * (200 - 20) + 1) + 20;
+        if (randomNumber % 2 === 0) {
+            console.log(`${randomNumber} is even`);
+        } else {
+            console.log(`${randomNumber} is odd`);
+        }
+    }
+}
+randomEvenOrOdd()
 
-//#4.1
-// function numPyramid(){
-//     for(let i = 1; i < 10; i++){
-//         console.log(`${i.toString().repeat(i)}`);
-//     }
-// }
-// numPyramid()
+// #4
+for (let i = 1; i <= 9; ++i) {
+    let result = [];
+    for (a = 1; a <= i; ++a){
+        result.push(i);
+    }
+    console.log(result.join(""));
+}
 
-//#5
-// for(let i = 100; i >= 5; i -= 5){
-//     console.log(i);
-// }
+// #4.1
+function numPyramid(){
+    for(let i = 1; i < 10; i++){
+        console.log(`${i.toString().repeat(i)}`);
+    }
+}
+numPyramid()
+
+// #5
+for(let i = 100; i >= 5; i -= 5){
+    console.log(i);
+}
