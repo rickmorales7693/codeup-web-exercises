3c10422d574467b470556a5d1d4f296b97101abd
Rick Morales <rick.morales7693@gmail.com>
2023-07-10 16:05:34 -0500

    added audio file to konami.js

--- js/konami.js
+++ js/konami.js
@@ -1,14 +1,14 @@
 "use strict";
 $(() => {
 
-    $(document).keyup(function(event){
+    $(document).keyup(function (event) {
         console.log(event.keyCode);
     });
 
-    let konamiCode = [38,38,40,40,37,39,37,39,66,65];
+    let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
     let position = 0;
 
-    $(document).keyup(function(e){
+    $(document).keyup(function (e) {
         if (e.keyCode === konamiCode[position]) {
             position++;
             if (position === konamiCode.length) {
@@ -20,10 +20,9 @@ $(() => {
         }
     });
 
-    function activateCode(){
-        alert("You won a brand new F250!");
-        // $('#container').css('background-color', 'red');
-        $('#header-name').html('Here is your new truck!')
+    function activateCode() {
+        alert("You are Correct!");
+        $('#header-name').html('!!!America!!!')
             .css('color', 'red')
             .css('font-size', '50px')
             .css('text-align', 'center')
@@ -31,8 +30,13 @@ $(() => {
             .css('text-shadow', '2px 2px 4px #000000')
             .css('text-decoration', 'underline');
         $('body')
-            .css('background-image', 'url("img/SuperDuty.jpeg")')
-            .css('background-size', 'cover');
+            .css('background-image', 'url("img/america2.gif")')
+            .css('background-size', 'cover')
+            // .css('background-music', 'url("audio/america.mp3")');
+        $('#audio').html('<audio autoplay loop><source src="audio/america.mp3" type="audio/mpeg"></audio>');
+
+
+
 
     }
 
@@ -43,67 +47,4 @@ $(() => {
 
 
 
-
 })
-
-    // alert($('#header').html());
-    //
-    // const aboutPara1 = $('#paragraph1').html();
-    // alert(aboutPara1);
-    //
-    // alert($('#paragraph2').html());
-    //
-    // $('.codeup').css('border', '1px solid red');
-    //
-    // $('li').css('font-size', '20px');
-    //
-    // $('h1, p, li').css('background-color', 'yellow');
-
-
-    // //function style example
-    // function hideUnorderedList() {
-    //     $('ul').hide();
-    // }
-    //
-    // $('h1')
-    //     .on('click', hideUnorderedList);
-
-
-    // $('h2')
-    //     .on('click', function () {
-    //         $(this).css('text-decoration', 'underline');
-    //     })
-    //     .on('dblclick', function () {
-    //         $(this).css('text-decoration', 'none');
-    //     })
-    //     .on('mouseenter', function (){
-    //         $(this).css('cursor', 'pointer');
-    //     })
-    //     .on('mouseleave', function (){
-    //         $(this).css('cursor', 'default');
-    //     });
-
-    // $('h1')
-    //     .on('click', function () {
-    //         $(this).css('background-color', 'green');
-    //     });
-    // $('p')
-    //     .on('dblclick', function () {
-    //         $('p').css('font-size', '18px');
-    //     });
-    // $('li')
-    //     .on('mouseenter', function () {
-    //         $(this).css('color', 'red');
-    //     })
-    //     .on('mouseleave', function () {
-    //         $(this).css('color', 'black');
-    //     });
-
-    // $('li').hover(
-    //     function () {
-    //         $(this).css('color', 'red');
-    //     },
-    //     function () {
-    //         $(this).css('color', 'black');
-    //     }
-    // );
