a9afece64351731fdf3e20955bbb3bcf53ea14d2
Rick Morales <rick.morales7693@gmail.com>
2023-07-10 09:00:49 -0500

    updated jquery_exercises.js

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,19 @@
 "use strict";
 
-window.onload = function (){
-    alert("The DOM has loaded!");
-}
+$(() => {
+
+    // alert($('#title-selector').html());
+    //
+    // const aboutPara1 = $('#paragraph1').html();
+    // alert(aboutPara1);
+    //
+    // alert($('#paragraph2').html());
+
+    $('.codeup').css('border', '1px solid red');
+
+    $('li').css('font-size', '20px');
+
+    $('h1, p, li').css('background-color', 'yellow');
+
+
+});
---
+++ js/jquery_selectors.js
@@ -0,0 +1,18 @@
+$(() => {
+
+    $('#id-container').css('background-color', 'red');
+
+    $('.title').css('color', 'purple');
+
+    $('li').css('color', 'pink');
+
+    // $('[data-id="5"]').html('I am the new text');
+
+    $('h1, p, .highlight').css('font-weight', 'bold');
+
+    $('*').css('outline', '1px solid lightblue').css('font-weight', 'bold');
+
+
+
+
+})
