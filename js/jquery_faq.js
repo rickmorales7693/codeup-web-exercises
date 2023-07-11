8210ec7c76fe1294702a2bcc23921186bbc59b1d
Rick Morales <rick.morales7693@gmail.com>
2023-07-11 14:36:54 -0500

    updated jquery_faq.js

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,11 +1,18 @@
 $(()=> {
 
+    $('dd').addClass('invisible');
 
+    $('.invisible').css('display', 'none');
 
+    $('body').append('<button class="btn btn-primary">Read More</button>');
 
+    $('button').on('click', function() {
+        $('.invisible').toggle();
+    });
 
-
-
+    $('dt').on('click', function() {
+        $(this).css('background-color', 'chartreuse');
+    });
 
 
 
