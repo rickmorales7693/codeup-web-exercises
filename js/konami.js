6f6e8e12abcfe319d5950ac296e0f6812817441b
Rick Morales <rick.morales7693@gmail.com>
2023-07-11 10:15:28 -0500

    updated code

--- js/konami.js
+++ js/konami.js
@@ -5,7 +5,14 @@ $(() => {
         console.log(event.keyCode);
     });
 
-    $('#header-name').css('text-align', 'center')
+    $('#header-name')
+        .css('text-align', 'center')
+        .css('color', 'white')
+
+    $('body')
+        .css('background-image', 'url("img/plaid_ludicrous_speed.webp")')
+        .css('background-size', 'cover')
+
 
     let konamiCode = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
     let position = 0;
