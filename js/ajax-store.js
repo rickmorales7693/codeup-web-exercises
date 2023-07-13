04becb4d8fe960b5aaeb026d548d5cb06d8f6380
Rick Morales <rick.morales7693@gmail.com>
2023-07-13 10:53:09 -0500

    updated ajax-store and added ajax-blog

---
+++ js/ajax-store.js
@@ -0,0 +1,42 @@
+$(()=> {
+
+    function refresh(){
+        $.ajax("/data/inventory.json").done(function (data) {
+            console.log(data);
+            let html = " ";
+            data.forEach(function (product) {
+                html += "<tr>";
+                html += "<td>" + product.title + "</td>";
+                html += "<td>" + product.quantity + "</td>";
+                html += "<td>" + product.price + "</td>";
+                html += "<td>" + product.categories + "</td>";
+                html += "</tr>";
+            });
+            $("#insertProducts").html(html);
+        });
+    }
+
+    refresh();
+
+    $("#refresh").click(function () {
+        refresh();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
