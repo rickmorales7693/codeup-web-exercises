d8a5395db4207bcb4af06efff8e73ecfd20c49b1
Rick Morales <rick.morales7693@gmail.com>
2023-07-13 15:31:36 -0500

    updated mapbox-api.js

--- js/mapbox-api.js
+++ js/mapbox-api.js
@@ -2,7 +2,8 @@ $(() => {
 
     //Global Variables
     const map = initializeMap();
-
+    const marker = createMarker();
+    const popup = createPopup();
 
     //Functions
     //Function that initializes the map
@@ -20,18 +21,71 @@ $(() => {
 
     }
 
+    //Function that creates a marker
+    function createMarker(){
+        return new mapboxgl.Marker()
+            .setLngLat([-98.4916, 29.4252])
+            .addTo(map);
+    }
+
+    //Function that creates a popup
+    function createPopup(){
+        return new mapboxgl.Popup()
+            .setLngLat([-98.4916, 29.4252])
+            .setHTML(`
+                <div>   
+                    <h1>Codeup</h1>
+                    <p>We can put anything we want</p>
+                </div>
+            `);
+    }
 
 
+    //Function that brings me to paris
+    function goToParis(){
+        geocode('Paris', MAPBOX_PROJECT).then((data) => {
+            console.log(data);
+            map.setCenter(data);
+
+        })
+    }
+
+
+    //Function that uses reverse geocoding
+    //Takes the coordinates from the center of te map and prints
+    function findAndPrintAddress(){
+        const coords = map.getCenter()
+        reverseGeocode(coords, MAPBOX_PROJECT).then((data) => {
+            console.log(data);
+            document.querySelector('h1').innerHTML = `${data}`;
+        });
+    }
+
+
+    //Function that uses geocode to take the string The Alamo, San Antonio and get coordinates to set a marker and popup
+    function markAlamo() {
+        geocode('The Alamo, San Antonio', MAPBOX_PROJECT).then((data) => {
+            const alamoPopup = new mapboxgl.Popup()
+                .setHTML('<p>Remember the Alamo</p>');
+            const alamoMarker = new mapboxgl.Marker()
+                .setLngLat(data)
+                .setPopup(alamoPopup)
+                .addTo(map);
+            alamoPopup.addTo(map);
+        });
+    }
 
     //Events
-
-
+    document.querySelector('#geocode-button').addEventListener('click', goToParis);
+    document.querySelector('#reverse-geocode-button').addEventListener('click', reverseGeocode());
+    document.querySelector('#find-address').addEventListener('click', findAndPrintAddress);
+    document.querySelector('#mark-alamo').addEventListener('click', markAlamo);
 
 
 
     //Runs when the program loads
     map.zoom(15);
-
+    marker.setPopup(popup);
 
 
 
