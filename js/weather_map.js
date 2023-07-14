1f47e384c74bd51ab4cd9cc24d832c40ac5e790d
Rick Morales <rick.morales7693@gmail.com>
2023-07-14 17:49:36 -0500

    added heaader to weather_map.html and copied code to weather_map.js

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,4 +1,4 @@
-(()=>{
+$(()=>{
 
     // Base URL for forecast API
     const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
@@ -29,6 +29,50 @@
     //Runs when the program loads
     map.setZoom(9);
 
+    //Function that Sets a marker and popup
+    function elizabethRestaurant() {
+        geocode('5251 Timberhill Dr, San Antonio TX 78238', MAPBOX_PROJECT).then((data) => {
+            const elizabethPopup = new mapboxgl.Popup()
+                .setHTML(`
+                    <div>   
+                        <h1>Elizabeth's Mexican Restaurant</h1>
+                        <p>5251 Timberhill Dr, San Antonio TX 78238</p>
+                    </div>
+                    `)
+            const elizabethMarker = new mapboxgl.Marker()
+                .setLngLat(data)
+                .setPopup(elizabethPopup)
+                .addTo(map)
+            elizabethPopup.addTo(map);
+
+            map.flyTo({
+                center: data,
+                zoom: 17,
+                speed: 1,
+                essential: true
+            })
+        });
+    }
+
+    // Add a text box for the user to enter an address that will use geocoding to center the map and place a marker on that location.
+    $('#search-button').click(function () {
+        const userInput = $('#search-input').val();
+        geocode(userInput, MAPBOX_PROJECT).then((data) => {
+            const popup = new mapboxgl.Popup()
+            const marker = new mapboxgl.Marker()
+                .setLngLat(data)
+                .setPopup(popup)
+                .addTo(map);
+            popup.addTo(map);
+
+            map.flyTo({
+                center: data,
+                zoom: 14,
+                speed: 1,
+                essential: true
+            })
+        });
+    });
 
 
 
@@ -36,4 +80,7 @@
 
 
 
-})();
+
+
+
+});
