a36ed9d5dda682a7e9b81497668dda3fa3b5e07e
Rick Morales <rick.morales7693@gmail.com>
2023-07-17 14:43:04 -0500

    got the cards to update with current weather

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,7 +1,11 @@
 $(() => {
 
+
+
+
     // Base URL for forecast API
     const OPEN_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
+
 
 
     // Simple way to create URL for request based on coordinates
@@ -9,6 +13,11 @@ $(() => {
         return `${OPEN_WEATHER_URL}?lat=${lat}&lon=${lon}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
     }
 
+    function getWeatherInfo(lat, lon){
+        $.ajax(getWeatherURL(lat, lon)).done((data) => {
+            createFiveCards(data);
+        });
+    }
 
     //Function that initializes the map
     const map = initializeMap();
@@ -27,7 +36,8 @@ $(() => {
 
 
     // Add a text box for the user to enter an address that will use geocoding to center the map and place a marker on that location.
-    $('#search-button').click(function () {
+    $('#search-button')
+        .click(function () {
         const userInput = $('#search-input').val();
         geocode(userInput, MAPBOX_PROJECT).then((data) => {
             const popup = new mapboxgl.Popup()
@@ -44,241 +54,95 @@ $(() => {
                 essential: true
             });
             getCurrentCity(data[0],data[1]);
+            createFiveCards();
         });
+
     });
 
 
-    //Function that shows the current city of the marker that was searched for
+
+    //Function that shows the current city and state of the marker that was searched for
     function getCurrentCity(lon, lat) {
         const url = getWeatherURL(lat, lon);
         $.get(url).done((data) => {
-            console.log(data);
             const currentCity = data.city.name;
             $('#current-city').html(currentCity)
         });
     }
 
 
-    // Function that gives a 24-hour forecast for the location of the marker
-    // function getTwentyFourHourForecast() {
-    //     const lat = map.getCenter().lat;
-    //     const lon = map.getCenter().lng;
-    //     const url = getWeatherURL(lat, lon);
-    //
-    //     $.get(url).done((data) => {
-    //         console.log(data);
-    //         const forecast = data.list;
-    //         const currentForecast = forecast.filter((item) => {
-    //             return item.dt_txt.includes(formattedDate);
-    //         });
-    //
-    //         currentForecast.forEach((forecast, index) => {
-    //             const cardId = `card${index + 1}`;
-    //             const cardContentElement = $(`#${cardId}-content`);
-    //
-    //             const forecastContent = `
-    //             <ul class="list-group list-group-flush">
-    //                 <li class="list-group-item">Temperature: ${forecast.main.temp}</li>
-    //                 <li class="list-group-item">Feels Like: ${forecast.main.feels_like}</li>
-    //                 <li class="list-group-item">Humidity: ${forecast.main.humidity}</li>
-    //                 <li class="list-group-item">Wind Speed: ${forecast.wind.speed}</li>
-    //                 <li class="list-group-item">Wind Direction: ${forecast.wind.deg}</li>
-    //                 <li class="list-group-item">Clouds: ${forecast.clouds.all}</li>
-    //                 <li class="list-group-item">Weather: ${forecast.weather[0].description}</li>
-    //             </ul>
-    //         `;
-    //
-    //             cardContentElement.html(forecastContent);
-    //         });
-    //     });
-    // }
-    //
-    // getTwentyFourHourForecast();
+    // New Function for 5 Cards
+    function createFiveCards() {
+        $('#cards').html('');
+        for (let i = 0; i < 5; i++) {
+            const cardId = `card${i + 1}`;
+            const cardContent = `<div id="${cardId}" class="card"></div>`;
+            $('#cards').append(cardContent);
+        }
+        getNextFiveDaysWeather();
+    }
+
+    function getNextFiveDaysWeather() {
+        const lat = map.getCenter().lat;
+        const lon = map.getCenter().lng;
+        const url = getWeatherURL(lat, lon);
+        const forecastArray = [];
+
+        $.get(url).done((data) => {
+            console.log(data);
+            const forecast = data.list;
+
+            forecastArray.push(...filterDailyForecasts(forecast));
+
+            forecastArray.forEach((forecastItem, index) => {
+                const forecastDateTime = new Date(forecastItem.dt * 1000);
+                const forecastDate = forecastDateTime.toLocaleDateString();
+                const forecastTime = forecastDateTime.toLocaleTimeString();
+                const forecastContent = `
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">Date: ${forecastDate} - Time: ${forecastTime}</li>
+                          <li class="list-group-item">Temperature: ${forecastItem.main.temp}</li>
+                          <li class="list-group-item">Feels Like: ${forecastItem.main.feels_like}</li>
+                          <li class="list-group-item">Humidity: ${forecastItem.main.humidity}</li>
+                          <li class="list-group-item">Wind Speed: ${forecastItem.wind.speed}</li>
+                          <li class="list-group-item">Wind Direction: ${forecastItem.wind.deg}</li>
+                          <li class="list-group-item">Clouds: ${forecastItem.clouds.all}</li>
+                          <li class="list-group-item">Weather: ${forecastItem.weather[0].description}</li>
+                        </ul>
+      `;
+
+                $(`#card${index + 1}`).append(forecastContent);
+            });
+        });
+    }
+    function filterDailyForecasts(forecast) {
+        return forecast.filter((item, index) => index % 8 === 0);
+    }
+
+    createFiveCards();
 
 
-    // Function that gives a 48-hour forecast for the location of the marker
-    // function getFortyEightHourForecast() {
-    //     const lat = map.getCenter().lat;
-    //     const lon = map.getCenter().lng;
-    //     const url = getWeatherURL(lat, lon);
-    //
-    //     $.get(url).done((data) => {
-    //         console.log(data);
-    //         const forecast = data.list;
-    //         const currentForecast = forecast.filter((forecast) => {
-    //             return forecast.dt_txt.includes(formattedDate);
-    //         });
-    //         console.log(currentForecast);
-    //         const currentForecastHTML = currentForecast.map((forecast) => {
-    //             return `
-    //             <div class="card">
-    //                 <div class="card-header" id="headingOne" currentDate="${formattedDate}">
-    //                     <h5 class="mb-0">
-    //                         <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
-    //                                 aria-expanded="true" aria-controls="collapseOne">
-    //                             ${forecast.dt_txt}
-    //                         </button>
-    //                     </h5>
-    //                 </div>
-    //                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-    //                      data-parent="#accordionExample">
-    //                     <div class="card-body">
-    //                         <ul>
-    //                             <li>Temperature: ${forecast.main.temp}</li>
-    //                             <li>Feels Like: ${forecast.main.feels_like}</li>
-    //                             <li>Humidity: ${forecast.main.humidity}</li>
-    //                             <li>Wind Speed: ${forecast.wind.speed}</li>
-    //                             <li>Wind Direction: ${forecast.wind.deg}</li>
-    //                             <li>Clouds: ${forecast.clouds.all}</li>
-    //                             <li>Weather: ${forecast.weather[0].description}</li>
-    //                         </ul>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //             `;
-    //         });
-    //         $('#accordionExample').html(currentForecastHTML);
-    //     });
-    // }
+    // Marker for the map
+    const marker = createMarker();
+    function createMarker() {
+        return new mapboxgl.Marker()
+            .setLngLat([-98.4916, 29.4252])
+            .addTo(map);
+    }
 
 
-    // Function that gives a 72-hour forecast for the location of the marker
-    // function getSeventyTwoHourForecast() {
-    //     const lat = map.getCenter().lat;
-    //     const lon = map.getCenter().lng;
-    //     const url = getWeatherURL(lat, lon);
-    //
-    //     $.get(url).done((data) => {
-    //         console.log(data);
-    //         const forecast = data.list;
-    //         const currentForecast = forecast.filter((forecast) => {
-    //             return forecast.dt_txt.includes(formattedDate);
-    //         });
-    //         console.log(currentForecast);
-    //         const currentForecastHTML = currentForecast.map((forecast) => {
-    //             return `
-    //             <div class="card">
-    //                 <div class="card-header" id="headingOne" currentDate="${formattedDate}">
-    //                     <h5 class="mb-0">
-    //                         <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
-    //                                 aria-expanded="true" aria-controls="collapseOne">
-    //                             ${forecast.dt_txt}
-    //                         </button>
-    //                     </h5>
-    //                 </div>
-    //                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-    //                      data-parent="#accordionExample">
-    //                     <div class="card-body">
-    //                         <ul>
-    //                             <li>Temperature: ${forecast.main.temp}</li>
-    //                             <li>Feels Like: ${forecast.main.feels_like}</li>
-    //                             <li>Humidity: ${forecast.main.humidity}</li>
-    //                             <li>Wind Speed: ${forecast.wind.speed}</li>
-    //                             <li>Wind Direction: ${forecast.wind.deg}</li>
-    //                             <li>Clouds: ${forecast.clouds.all}</li>
-    //                             <li>Weather: ${forecast.weather[0].description}</li>
-    //                         </ul>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //             `;
-    //         });
-    //         $('#accordionExample').html(currentForecastHTML);
-    //     });
-    // }
+    //Click anywhere and get the weather for that location
+    map.on('click', (e)=>{
+        const lat = e.lngLat.lat;
+        const lon = e.lngLat.lng;
+        getWeatherInfo(e.lngLat.lat, e.lngLat.lng);
+        console.log(e.lngLat.lat, e.lngLat.lng);
+        marker.setLngLat([lon, lat]);
+
+    });
 
 
-    // Function that gives a 96-hour forecast for the location of the marker
-    // function getNinetySixHourForecast() {
-    //     const lat = map.getCenter().lat;
-    //     const lon = map.getCenter().lng;
-    //     const url = getWeatherURL(lat, lon);
-    //
-    //     $.get(url).done((data) => {
-    //         console.log(data);
-    //         const forecast = data.list;
-    //         const currentForecast = forecast.filter((forecast) => {
-    //             return forecast.dt_txt.includes(formattedDate);
-    //         });
-    //         console.log(currentForecast);
-    //         const currentForecastHTML = currentForecast.map((forecast) => {
-    //             return `
-    //             <div class="card">
-    //                 <div class="card-header" id="headingOne" currentDate="${formattedDate}">
-    //                     <h5 class="mb-0">
-    //                         <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
-    //                                 aria-expanded="true" aria-controls="collapseOne">
-    //                             ${forecast.dt_txt}
-    //                         </button>
-    //                     </h5>
-    //                 </div>
-    //                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-    //                      data-parent="#accordionExample">
-    //                     <div class="card-body">
-    //                         <ul>
-    //                             <li>Temperature: ${forecast.main.temp}</li>
-    //                             <li>Feels Like: ${forecast.main.feels_like}</li>
-    //                             <li>Humidity: ${forecast.main.humidity}</li>
-    //                             <li>Wind Speed: ${forecast.wind.speed}</li>
-    //                             <li>Wind Direction: ${forecast.wind.deg}</li>
-    //                             <li>Clouds: ${forecast.clouds.all}</li>
-    //                             <li>Weather: ${forecast.weather[0].description}</li>
-    //                         </ul>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //             `;
-    //         });
-    //         $('#accordionExample').html(currentForecastHTML);
-    //     });
-    // }
-
-
-    // Function that gives a 120-hour forecast for the location of the marker
-    // function getFiveDayForecast() {
-    //     const lat = map.getCenter().lat;
-    //     const lon = map.getCenter().lng;
-    //     const url = getWeatherURL(lat, lon);
-    //
-    //     $.get(url).done((data) => {
-    //         console.log(data);
-    //         const forecast = data.list;
-    //         const currentForecast = forecast.filter((forecast) => {
-    //             return forecast.dt_txt.includes(formattedDate);
-    //         });
-    //         console.log(currentForecast);
-    //         const currentForecastHTML = currentForecast.map((forecast) => {
-    //             return `
-    //             <div class="card">
-    //                 <div class="card-header" id="headingOne" currentDate="${formattedDate}">
-    //                     <h5 class="mb-0">
-    //                         <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"
-    //                                 aria-expanded="true" aria-controls="collapseOne">
-    //                             ${forecast.dt_txt}
-    //                         </button>
-    //                     </h5>
-    //                 </div>
-    //                 <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-    //                      data-parent="#accordionExample">
-    //                     <div class="card-body">
-    //                         <ul>
-    //                             <li>Temperature: ${forecast.main.temp}</li>
-    //                             <li>Feels Like: ${forecast.main.feels_like}</li>
-    //                             <li>Humidity: ${forecast.main.humidity}</li>
-    //                             <li>Wind Speed: ${forecast.wind.speed}</li>
-    //                             <li>Wind Direction: ${forecast.wind.deg}</li>
-    //                             <li>Clouds: ${forecast.clouds.all}</li>
-    //                             <li>Weather: ${forecast.weather[0].description}</li>
-    //                         </ul>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //             `;
-    //         });
-    //         $('#accordionExample').html(currentForecastHTML);
-    //     });
-    // }
-
-
+    //Get the weather for San Antonio on the page load
+    getWeatherInfo(29.4252, -98.4916)
 
 });
