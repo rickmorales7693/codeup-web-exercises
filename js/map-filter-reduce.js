7f576f18af2db3cd443738fad44ae8e430994688
Rick Morales <rick.morales7693@gmail.com>
2023-07-20 09:47:23 -0500

    added dom-manipulation-warmups.html

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -38,35 +38,22 @@ const users = [
 
 
 //Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
-let threeLanguages = users.filter(user => user.languages.length >= 3);
+const threeLanguages = users.filter(user => user.languages.length >= 3);
 
 //Use .map to create an array of strings where each element is a user's email address
-let userEmails = users.map(user => user.email);
+const userEmails = users.map(user => user.email);
 
 //Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
-let totalYears = users.reduce((total, user) => {
-    return total + user.yearsOfExperience;
-}, 0);
+const totalYears = users.reduce((total, user) => total + user.yearsOfExperience, 0);
 
 //Use .reduce to get the longest email from the list of users.
-let longestEmail = users.reduce((longest, user) => {
-    if (user.email.length > longest.length) {
-        longest = user.email;
-    }
-    return longest;
-}, "");
+const longestEmail = users.reduce((longest, user) => user.email.length > longest.length ? user.email : longest, "");
 
 //Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
-let userNames = users.reduce((names, user) => {
-    return names + user.name + ", ";
-}, "Your instructors are: ");
+const userNames = users.reduce((names, user) => names + user.name + ", ", "Your instructors are: ");
 
 //Use .reduce to get the unique list of languages from the list of users.
-let uniqueLanguages = users.reduce((languages, user) => {
-    for (let language of user.languages) {
-        if (!languages.includes(language)) {
-            languages.push(language);
-        }
-    }
+const uniqueLanguages = users.reduce((languages, user) => {
+    user.languages.forEach(language => languages.add(language));
     return languages;
-}, []);
+}, new Set());
