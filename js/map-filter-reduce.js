dc798d46de9b15525a1005a23db0e7cab5065e50
Rick Morales <rick.morales7693@gmail.com>
2023-07-21 09:54:04 -0500

    updated map-filter-reduce.js

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -40,27 +40,41 @@ const users = [
 //Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
 const threeLanguages = users.filter(user => user.languages.length >= 3);
 
-console.log(threeLanguages);
+console.log('User with 3 languages => ', threeLanguages);
+
 
 //Use .map to create an array of strings where each element is a user's email address
 const userEmails = users.map(user => user.email);
 
-console.log(userEmails);
+console.log('User with email address => ', userEmails);
 
-//Use .reduce to get the total years of experience from the list of users. Once you get the total of years you can use the result to calculate the average.
-const totalYears = users.reduce((total, user) => total + user.yearsOfExperience, 0);
 
-console.log(totalYears);
+// //Use .reduce to get the total years of experience from the list of users.
+// //Once you get the total of years, you can use the result to calculate the average.
+// const totalYears = users.reduce((total, user) => total + user.yearsOfExperience, 0);
+//
+// console.log('Total years of experience => ', totalYears);
+
 
 //Use .reduce to get the longest email from the list of users.
-const longestEmail = users.reduce((longest, user) => user.email.length > longest.length ? user.email : longest, "");
+// const longestEmail = users.reduce((longest, user) => user.email.length > longest.length ? user.email : longest, "");
+//
+// console.log(longestEmail);
+const longestEmail = users.reduce((accumulator, user, index)=>{
+    const emailLength = user.email.length;
+    const accLength = accumulator.length;
+    if (emailLength > accLength){
+        accumulator = user.email;
+    }
+    return accumulator;
+}, '');
 
-console.log(longestEmail);
+console.log('Longest Email => ', longestEmail)
 
 //Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 const userNames = users.reduce((names, user) => names + user.name + ", ", "Your instructors are: ");
+console.log('User Names =>', userNames);
 
-console.log(userNames);
 
 //Use .reduce to get the unique list of languages from the list of users.
 const uniqueLanguages = users.reduce((languages, user) => {
@@ -68,4 +82,4 @@ const uniqueLanguages = users.reduce((languages, user) => {
     return languages;
 }, new Set());
 
-console.log(uniqueLanguages);
+console.log('Users Unique List => ' ,uniqueLanguages);
