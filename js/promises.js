c35977298c072fa200a4c9c113c9d7388f23be3a
Rick Morales <rick.morales7693@gmail.com>
2023-07-21 15:44:07 -0500

    asynced the promises.js exercise

--- js/promises.js
+++ js/promises.js
@@ -1,32 +1,56 @@
-const userName = (id) => {
+// const userName = (id) => {
+//
+//     const url = `https://api.github.com/users/${id}/events`
+//     console.log(url);
+//
+//     const options = {
+//         method: 'GET',
+//         headers: {
+//             "Authorization": `token ${PROMISE_KEYS}`,
+//         }
+//     }
+//     console.log(options);
+//
+//     return fetch(url, options)
+//         .then((response)=>{
+//             return response.json();
+//         })
+//         .catch(error => {
+//             console.log(error.message);
+//         })
+// }
 
-    const url = `https://api.github.com/users/${id}/events`
-    console.log(url);
 
-    const options = {
-        method: 'GET',
-        headers: {
-            "Authorization": `token ${PROMISE_KEYS}`,
+const userName = async (id = 1) => {
+    try {
+        const url = `https://api.github.com/users/${id}/events`;
+        const options = {
+            method: "GET",
+            headers: {
+                "Authorization": `token ${PROMISE_KEYS}`,
+            }
         }
+        const response = await fetch(url, options);
+        const person = await response.json();
+        return person;
+    } catch(error){
+        console.log(error);
     }
-    console.log(options);
-
-    return fetch(url, options)
-        .then((response)=>{
-            return response.json();
-        })
-        .catch(error => {
-            console.log(error.message);
-        })
 }
 
 
-(() => { // IIFE (Immediately Invoked Function Expression)
 
-    userName('rickmorales7693').then((user) => {
-        console.log(user);
-        console.log(user[0].actor.login,  user[0].created_at);
-    });
+
+(async () => { // IIFE (Immediately Invoked Function Expression)
+
+    // userName('rickmorales7693').then((user) => {
+    //     console.log(user);
+    //     console.log(user[0].actor.login,  user[0].created_at);
+    // });
+
+    const user = await userName('rickmorales7693')
+    console.log(user);
+    console.log(user[0].actor.login,  user[0].created_at);
 
 
 })();
