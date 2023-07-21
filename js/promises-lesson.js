110a12e7021e65e37086f303b976e8d0778235cf
Rick Morales <rick.morales7693@gmail.com>
2023-07-21 11:08:55 -0500

    added new functions too promises-lesson.js

--- js/promises-lesson.js
+++ js/promises-lesson.js
@@ -1,4 +1,57 @@
+const getPerson = (id) => {
+    const url = `https://swapi.dev/api/people/${id}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }
+     return fetch(url, options)
+        .then((response)=>{
+            return response.json();
+        })
+        //  .then((data)=>{
+        // console.log(data);
+
+}
+
 (()=>{ // IIFE (Immediately Invoked Function Expression)
+
+    const luke = getPerson(1).then((data)=>{
+        console.log(data);
+        const url = data.films[0];
+        const options = {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        };
+        fetch(url, options)
+            .then((response)=>{
+                return response.json();
+            })
+            .then((film)=>{
+                console.log(film);
+            })
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
