290c4a596a6d8f593ba71e52dbd1192540f19fdf
Rick Morales <rick.morales7693@gmail.com>
2023-07-22 18:34:08 -0500

    updated promises-async.html, js and css to make it work

--- js/promises-async.js
+++ js/promises-async.js
@@ -2,12 +2,10 @@
 // Remember, it's just syntactic sugar!
 // In the end, it's doing the same thing as the .then() method
 // WRITE CODE THAT WORKS!!!
-
 // test url: "https://swapi.dev/api/films/1/"
 //tradition declaration
 // async function getFilm(url){
 // }
-
 //arrow declaration
 const getPerson = async (id = 1) => {
     try {
@@ -41,11 +39,52 @@ const getFilm = async (url) => {
     }
 
 }
+const getPeople = async () => {
+    const url = 'https://swapi.dev/api/people/';
+    const options = {
+        method: 'GET',
+        header: {
+            'Content-Type': 'application/json'
+        }
+    }
+    const response = await fetch(url, options);
+    const people = await response.json();
+    return people;
+}
 
 (async ()=>{
-    const luke = await getPerson(1);
-    console.log(luke);
-    const firstFilm = await getFilm(luke.films[0]);
-    console.log(firstFilm);
+
+    const results = await getPeople();
+    console.log(results);
+    const people = results.results;
+    console.log('Just the people => ', people);
+    const peopleRows = people.map((person) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <div class="d-flex gap-10 align-center">
+                    <img class="character-image" src="https://via.placeholder.com/50x50">
+                    <p class="character-name">${person.name}</p>
+                </div>
+            </td>
+            <td>${person.height}</td>
+            <td>${person.mass}</td>
+            <td>${person.hair_color}</td>
+            <td>${person.skin_color}</td>
+            <td>${person.eye_color}</td>
+            <td>${person.birth_year}</td>
+            <td>${person.gender}</td>
+            <td><button>delete</button></td>
+        `
+        let button = row.querySelector('button');
+        button.addEventListener('click', ()=>{
+            row.remove();
+        });
+        return row;
+    });
+    for(let person of peopleRows) {
+        document.querySelector('#characters tbody').appendChild(person);
+    }
+
 
 })();
